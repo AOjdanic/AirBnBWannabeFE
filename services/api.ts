8b4c118f4473e6e0220a1a5e31c524e89a55668a
Aleksa Ojdanic <aojdanic@Aleksas-MacBook-Air.local>
2024-12-29 03:15:13 +0100

    update nextjs to latest version

--- services/api.ts
+++
@@ -1,9 +0,0 @@
-import axios, { AxiosInstance } from "axios";
-
-axios.defaults.withCredentials = true;
-
-const Api: AxiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/v1",
-});
-
-export { Api };
